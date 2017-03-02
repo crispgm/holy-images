3a47260401c2316746c189e2457a4d4990991133
David Zhang <crispgm@gmail.com>
2017-03-02 14:12:16 +0800

    Fix like

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -19,9 +19,11 @@ class ImageController < ApplicationController
   def like
     image_id = params[:id]
     # check existence
-    begin
-      @like = Like.find_by(image_id: image_id, user_id: 1) # TODO
+    @like = Like.find_by(image_id: image_id, user_id: 1) # TODO
+    logger.info("aaaa")
+    logger.info(@like.inspect)
 
+    unless @like == nil
       if @like.status == Like::STATUS_LIKE
         @like.status = Like::STATUS_UNLIKE
       else
@@ -38,19 +40,24 @@ class ImageController < ApplicationController
           end
         end
       end
-    rescue ActiveRecord::RecordNotFound
-      # add new
-      @initial_like = Like.new
-      # TODO
-      @initial_like.user = User.find(1)
-      @initial_like.image = Image.find(image_id)
-      @initial_like.status = Like::STATUS_LIKE
-      @initial_like.save
+    else
+      new_like(image_id, 1)
       respond_to do |format|
         format.json do
           render json: @initial_like
         end
       end
     end
+  end
+
+  private
+  def new_like(image_id, user_id)
+    # add new
+    @initial_like = Like.new
+    # TODO
+    @initial_like.user = User.find(user_id)
+    @initial_like.image = Image.find(image_id)
+    @initial_like.status = Like::STATUS_LIKE
+    @initial_like.save
   end
 end
