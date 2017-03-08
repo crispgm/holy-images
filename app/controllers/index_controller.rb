97ffbedd001c7a7098a53c5d9e7aeab350a82358
David Zhang <crispgm@gmail.com>
2017-03-08 19:45:30 +0800

    Add pagination

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,15 @@
 class IndexController < ApplicationController
   def index
-    @images = Image.all
+    page = params[:p].to_i || 1
+    page = 1 if page <= 0
+    start = (page - 1) * 10
+    total = Image.count
+    @images = Image.limit(10).offset(start)
+
+    @page = {
+      :total => total,
+      :cur_page => page,
+      :total_page => (total.to_f/10.to_f).ceil
+    }
   end
 end
