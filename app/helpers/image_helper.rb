a44b07c69409d9954646f6bdc0624f1f9ea9519d
David Zhang <crispgm@gmail.com>
2017-03-22 17:38:28 +0800

    Monkey patch: add this_year?

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,5 +1,13 @@
 module ImageHelper
   def extract_exif_value(input)
-  	input.split("(").at(1).split(")").at(0)
+    input.split("(").at(1).split(")").at(0)
+  end
+end
+
+module DateAndTime
+  module Calculations
+    def this_year?
+      year == ::Date.current.year
+    end
   end
 end
