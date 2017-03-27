b41612d3d2e9782c3850b76e63d4509e2d24320a
David Zhang <crispgm@gmail.com>
2017-03-27 16:08:59 +0800

    Fix unusual case

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,7 +8,7 @@ class Image < ApplicationRecord
   validates :title, length: {minimum: 2}, presence: true
   has_attached_file :img_file, styles: { thumbnail: "640x640#" }
   validates_attachment :img_file,
-    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] } if Rails.application.config.validates_paperclip_attachment
   validate :url_or_file_presence
 
   default_scope do
--- config/initializers/paperclip.rb
+++
@@ -1,1 +0,0 @@
-Paperclip.options[:content_type_mappings] = { jpeg: "image/jpeg", jpg: "image/jpeg" }
