b18707b1bc69a7e4f4ec564f775d7cae1ec3eb1b
David Zhang <crispgm@gmail.com>
2017-03-30 17:43:52 +0800

    I18N Support for en/zh-CN

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
-  get "index" => "index#index"
+  scope "(:locale)", locale: /en|zh-CN/ do
+    get "index" => "index#index"
 
-  get "upload" => "image#upload"
-  get "image/:id" => "image#item"
-  post "image/create" => "image#create"
-  post "image/like/:id" => "image#like"
+    get "upload" => "image#upload"
+    get "image/:id" => "image#item"
+    post "image/create" => "image#create"
+    post "image/like/:id" => "image#like"
 
-  get "explore" => "image#explore"
+    get "explore" => "image#explore"
 
-  get "user/new" => "user#new"
-  get "user/sign-in" => "user#sign_in"
-  post "user/create" => "user#create"
-  post "user/login" => "user#login"
-  post "user/logout" => "user#logout"
-  get "u/:name" => "user#info"
-  get "u/:name/invitation" => "user#invite"
+    get "user/new" => "user#new"
+    get "user/sign-in" => "user#sign_in"
+    post "user/create" => "user#create"
+    post "user/login" => "user#login"
+    post "user/logout" => "user#logout"
+    get "u/:name" => "user#info"
+    get "u/:name/invitation" => "user#invite"
 
-  root "index#index"
+    root "index#index"
+  end
 end
