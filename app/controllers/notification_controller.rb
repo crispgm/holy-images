9ed10430806e8cccb3ad2ed1629161024308bc04
David Zhang <crispgm@gmail.com>
2017-05-27 12:19:02 +0800

    Bugfix: wrong redirection

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -21,7 +21,23 @@ class NotificationController < ApplicationController
     else
       id = id.to_i
       result = Notification.where(status: 0, user_id: user.id, id: id).update(status: 1)
-      redirect_to "/image/#{id}#comments"
+      notif = Notification.find(id)
+      target_id = case notif.event_type
+      when 1
+        target_id = notif.like.image_id
+      when 2
+        target_id = notif.comment.image_id
+      else
+        nil
+      end
+      redirect_url = "/image/#{target_id}"
+      redirect_url << "#comments" if notif.event_type == 2
+
+      unless target_id.nil?
+        redirect_to redirect_url
+      else
+        redirect_to root_url
+      end
     end
   end
 
